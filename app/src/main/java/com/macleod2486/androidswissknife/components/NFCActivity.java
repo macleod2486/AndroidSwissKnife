e9755b1811f394265dc1e31ee926a99069b0991d
macleod2486 <macleod2486@gmail.com>
2017-04-08 03:39:11 -0500

    Added in basic write and read for mifareultralight

--- app/src/main/java/com/macleod2486/androidswissknife/components/NFCActivity.java
+++ app/src/main/java/com/macleod2486/androidswissknife/components/NFCActivity.java
@@ -24,6 +24,7 @@ import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
+import android.nfc.tech.MifareUltralight;
 import android.nfc.tech.Ndef;
 import android.nfc.tech.NdefFormatable;
 import android.os.Bundle;
@@ -34,6 +35,7 @@ import android.widget.TextView;
 import com.macleod2486.androidswissknife.R;
 
 import java.io.ByteArrayOutputStream;
+import java.nio.charset.Charset;
 import java.util.Locale;
 
 public class NFCActivity extends Activity
@@ -85,14 +87,20 @@ public class NFCActivity extends Activity
             {
                 Log.i("NFCActivity","Tech list "+tag.getTechList()[index]);
 
-                switch (tag.getTechList()[index])
+                techLoop: switch (tag.getTechList()[index])
                 {
                     case "Ndef":
                     case "NdefFormatable":
                     {
                         readNDEFtag(tag, intent);
                         validTech = true;
-                        break;
+                        break techLoop;
+                    }
+                    case "android.nfc.tech.MifareUltralight":
+                    {
+                        readMifareUltralight(tag);
+                        validTech = true;
+                        break techLoop;
                     }
                     default:
                     {
@@ -116,14 +124,18 @@ public class NFCActivity extends Activity
             {
                 Log.i("NFCActivity","Tech list "+tag.getTechList()[index]);
 
-                switch (tag.getTechList()[index])
+                techLoop: switch (tag.getTechList()[index])
                 {
                     case "Ndef":
                     case "NdefFormatable":
                     {
                         writeNDEFtag(tag, bundleMessage);
                         validTech = true;
-                        break;
+                        break techLoop;
+                    }
+                    case "android.nfc.tech.MifareUltralight":
+                    {
+                        writeMifareUltralight(tag, bundleMessage);
                     }
                     default:
                     {
@@ -244,6 +256,80 @@ public class NFCActivity extends Activity
         catch(Exception e)
         {
             tagResult.append("\nTag failed to write");
+            e.printStackTrace();
+        }
+    }
+
+    private void readMifareUltralight(Tag tag)
+    {
+        try
+        {
+            MifareUltralight mifareUltralightTag = MifareUltralight.get(tag);
+            mifareUltralightTag.connect();
+
+            if(mifareUltralightTag.isConnected())
+            {
+                byte[] result;
+                String stringResult;
+                for(int index = 4; index < 16; index++)
+                {
+                    result = mifareUltralightTag.readPages(index);
+                    stringResult = new String(result, Charset.forName("US-ASCII"));
+                    tagResult.append(stringResult);
+                }
+            }
+            else
+            {
+                tagResult.append("Error reading tag");
+            }
+
+            mifareUltralightTag.close();
+        }
+        catch (Exception e)
+        {
+            tagResult.append("Error reading tag");
+            e.printStackTrace();
+        }
+    }
+
+    private void writeMifareUltralight(Tag tag, String bundleMessage)
+    {
+        try
+        {
+            MifareUltralight mifareUltralightTag = MifareUltralight.get(tag);
+            mifareUltralightTag.connect();
+
+            if (mifareUltralightTag.isConnected())
+            {
+                double numberOfPages = bundleMessage.length() / 4;
+
+                if(numberOfPages % 4 != 0) numberOfPages +=1;
+                if(numberOfPages > 12) numberOfPages = 12;
+
+                int indexShift = 0;
+                String messageToWrite;
+
+                for(int index = 0; index < numberOfPages; index++)
+                {
+                    messageToWrite = "";
+
+                    for (int secIndex = 0; secIndex < 4 && secIndex + indexShift < bundleMessage.length(); secIndex++)
+                    {
+                        messageToWrite = messageToWrite + bundleMessage.charAt(secIndex + indexShift);
+                    }
+
+                    Log.i("NFCActivity","Message "+messageToWrite + " Length "+messageToWrite.length() + " indexShift "+indexShift + " index "+index);
+
+                    mifareUltralightTag.writePage(index + 4, messageToWrite.getBytes(Charset.forName("US-ASCII")));
+                    indexShift += 4;
+                }
+            }
+
+            mifareUltralightTag.close();
+        }
+        catch(Exception e)
+        {
+            tagResult.append("Error writing tag");
             e.printStackTrace();
         }
     }
